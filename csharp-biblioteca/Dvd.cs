4efe0d628c905f54e7abdffce1e030c479f4123b
KatiaLosignore <katialos.kl@gmail.com>
2023-09-11 15:24:27 +0200

    added class Dvd

---
+++ csharp-biblioteca/Dvd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Dvd : Document
+    {
+        // PROPRIETA'
+        public int Duration { get; set; }
+
+        // COSTRUTTORE
+        public Dvd(string code, string title, int year, string category, int position, string author, int duration) : base(code, title, year, category, position, author)
+        {
+            this.Duration = duration;
+        }
+
+
+        // METODI
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Duration: {this.Duration}");
+        }
+
+
+    }
+}
